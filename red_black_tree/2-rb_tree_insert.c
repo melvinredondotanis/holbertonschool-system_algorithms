2df6c4717d8d0eac89707ea4827a7a9374d6b20d
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-06-26 14:13:16 +0200

    remove redundant rb_tree_node function from rb_tree_insert.c

--- red_black_tree/2-rb_tree_insert.c
+++ red_black_tree/2-rb_tree_insert.c
@@ -2,31 +2,6 @@
 #include "rb_trees.h"
 
 /**
- * rb_tree_node - Creates a new Red-Black Tree node
- * @parent: Pointer to the parent node
- * @value: Value to store in the new node
- * @color: Color of the new node
- *
- * Return: Pointer to the new node, or NULL on failure
- */
-rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
-{
-    rb_tree_t *new_node;
-
-    new_node = malloc(sizeof(rb_tree_t));
-    if (!new_node)
-        return (NULL);
-
-    new_node->n = value;
-    new_node->color = color;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    return (new_node);
-}
-
-/**
  * rotate_left - Performs left rotation
  * @tree: Double pointer to root
  * @node: Node to rotate
