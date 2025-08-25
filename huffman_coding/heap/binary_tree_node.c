da66ca7a41a40e59cb62bf958ed1f563f778fada
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-08-25 14:02:42 +0200

    fix: add missing documentation for binary_tree_node function

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -1,6 +1,12 @@
 #include "heap.h"
 #include <stdlib.h>
 
+/**
+ * binary_tree_node - creates new node in binary tree
+ * @parent: pointer to parent or NULL
+ * @data: void pointer to data for this node
+ * Return: pointer to new node or NULL on error
+ */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
 {
 	binary_tree_node_t *new_node = calloc(1, sizeof(*new_node));
